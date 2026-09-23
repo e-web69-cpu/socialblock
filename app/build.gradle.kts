b05332bf3fef908ecdf9eeb5a1ce541ee0f929f2
e-web69-cpu <etemez.erneszt@gmail.com>
2026-09-23 19:53:20 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.socialblock"
         minSdk = 26
         targetSdk = 34
-                                        versionCode = 9
-                                        versionName = "0.9"
+                                                versionCode = 10
+                                                versionName = "0.10"
     }
 
     signingConfigs {
